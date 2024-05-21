2693a772f0d0a0a879dee4174c5691711c1d90e8
itoutamami <itoutamami1114@gmail.com>
2024-05-22 06:15:59 +0900

    アコーディオン

---
+++ sample.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.question').click(function() {
+      $(this).toggleClass('open');
+      $(this).next().slideToggle();
+      $('.question').not($(this)).next().slideUp(); 
+      $('.question').not($(this)).removeClass('open'); 
+    });
+  });
